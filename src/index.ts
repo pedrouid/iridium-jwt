153d922af6cb0cfc26177ead514688434f808006
Pedro Gomes <pedrogomes94@gmail.com>
2022-06-20 16:34:28 +0200

    fix issuer

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import KeyDIDResolver from "key-did-resolver";
 import { Resolver } from "did-resolver";
 
 import * as ed25519 from "@stablelib/ed25519";
+import { concat } from "uint8arrays/concat";
 import { toString } from "uint8arrays/to-string";
 import { fromString } from "uint8arrays/from-string";
 import { safeJsonParse, safeJsonStringify } from "@walletconnect/safe-json";
@@ -48,7 +49,9 @@ const DID_METHOD = "key";
 
 const MULTICODEC_ED25519_ENCODING = "base58btc";
 
-const MULTICODEC_ED25519_HEADER = "z6Mk";
+const MULTICODEC_ED25519_BASE = "z";
+
+const MULTICODEC_ED25519_HEADER = "K36";
 
 const MULTICODEC_ED25519_LENGTH = 32;
 
@@ -66,9 +69,13 @@ export function encodeJSON(val: any): string {
 }
 
 export function encodeIss(publicKey: Uint8Array): string {
+  const header = fromString(
+    MULTICODEC_ED25519_HEADER,
+    MULTICODEC_ED25519_ENCODING
+  );
   const multicodec =
-    MULTICODEC_ED25519_HEADER +
-    toString(publicKey, MULTICODEC_ED25519_ENCODING);
+    MULTICODEC_ED25519_BASE +
+    toString(concat([header, publicKey]), MULTICODEC_ED25519_ENCODING);
   return [DID_PREFIX, DID_METHOD, multicodec].join(DID_DELIMITER);
 }
 
@@ -77,14 +84,16 @@ export function decodeIss(issuer: string): Uint8Array {
   if (prefix !== DID_PREFIX || method !== DID_METHOD) {
     throw new Error(`Issuer must be a DID with method "key"`);
   }
-  const keyType = multicodec.slice(0, 4);
-  if (keyType !== MULTICODEC_ED25519_HEADER) {
+  const base = multicodec.slice(0, 1);
+  if (base !== MULTICODEC_ED25519_BASE) {
+    throw new Error(`Issuer must be a key in mulicodec format`);
+  }
+  const bytes = fromString(multicodec.slice(1), MULTICODEC_ED25519_ENCODING);
+  const type = toString(bytes.slice(0, 2), MULTICODEC_ED25519_ENCODING);
+  if (type !== MULTICODEC_ED25519_HEADER) {
     throw new Error(`Issuer must be a public key with type "Ed25519"`);
   }
-  const publicKey = fromString(
-    multicodec.slice(4),
-    MULTICODEC_ED25519_ENCODING
-  );
+  const publicKey = bytes.slice(2);
   if (publicKey.length !== MULTICODEC_ED25519_LENGTH) {
     throw new Error(`Issuer must be a public key with length 32 bytes`);
   }
@@ -141,9 +150,6 @@ export async function signJWT(subject: string, keyPair: ed25519.KeyPair) {
 
 export async function verifyJWT(jwt: string) {
   const { header, payload, signature } = decodeJWT(jwt);
-  console.log("internal", "header", header);
-  console.log("internal", "payload", payload);
-  console.log("internal", "signature", signature);
   if (header.alg !== JWT_IRIDIUM_ALG || header.typ !== JWT_IRIDIUM_TYP) {
     throw new Error("JWT must use EdDSA algorithm");
   }
@@ -181,11 +187,8 @@ async function test() {
   console.log("decoded", decoded);
   const keyDidResolver = KeyDIDResolver.getResolver();
   const resolver = new Resolver(keyDidResolver);
-  // const response = didJWT.verifyJWT(jwt, { resolver })
-  // console.log('response', response)
-  const issuer = decoded.payload.iss || "";
-  const doc = await resolver.resolve(issuer);
-  console.log("doc", doc);
+  const response = await didJWT.verifyJWT(jwt, { resolver });
+  console.log("response", response);
 }
 
 test();
